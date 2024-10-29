d4a0b6e6bc93f5a3c5008a0e77136751ac560ae7
Ismail Almaci <ia@conscia.com>
2024-10-29 18:06:12 +0100

    update

---
+++ SemanticKernelWorkshop/ex-4-native-functions/CharacterCountPlugin.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+public class CharacterCountPlugin
+{
+    [KernelFunction, Description(@"Count the amount of 'r's' in a string.")]
+    public Task<string> GetRCharacters(string input)
+    {
+        int count = input.Count(c => c == 'r');
+        return Task.FromResult($"The amount of 'r's' in the input is: {count}");
+    }
+}
