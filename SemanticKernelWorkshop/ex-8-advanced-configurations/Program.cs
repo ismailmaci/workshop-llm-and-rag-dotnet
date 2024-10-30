3b7512fb69bf9af24a6e87915e8bc294d486e32e
Ismail Almaci <ia@conscia.com>
2024-10-30 15:57:48 +0100

    ex8

---
+++ SemanticKernelWorkshop/ex-8-advanced-configurations/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SemanticKernel;
+using Kernel = Microsoft.SemanticKernel.Kernel;
+
+var (deploymentName, endpoint, apiKey) = LlmService.LlmService.LoadSettings();
+PromptExecutionSettings settings = new()
+        {
+            ExtensionData = new Dictionary<string, object>()
+            {
+                { "Temperature", 2 },
+                { "TopP", 0.5 },
+                { "MaxTokens", 1024 },
+                { "FormatSetting", "json_schema" }
+            }
+        };
+
+Kernel kernel = Kernel.CreateBuilder()
+    .AddAzureOpenAIChatCompletion(
+        deploymentName: deploymentName,
+        endpoint: endpoint,
+        apiKey: apiKey)
+    .Build();
+
+Console.Write("You: ");
+var userInput = Console.ReadLine() ?? string.Empty;
+await foreach (var update in kernel.InvokePromptStreamingAsync(userInput, new KernelArguments(settings)))
+{
+    await Task.Delay(50);
+    Console.Write(update);
+}
